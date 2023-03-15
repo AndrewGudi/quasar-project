ac8028dbcda47a4f07295cbc18595c60c065f544
AndrewGudi <jamesbanner1225@gmail.com>
2023-03-15 10:15:12 +0200

    initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+q-layout
+  q-header(height="80px" )
+    .content
+      q-img(src="../assets/logo.png" alt="logo" width="171px" height="36px")
+  q-page-container
+    router-view
+</template>
+
+<script setup lang="ts">
+</script>
+<style lang="scss">
+</style>
