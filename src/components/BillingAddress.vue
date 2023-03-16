b1d613c2e05b5fc2aa3ddd7c8720d83dbb44f5d9
AndrewGudi <jamesbanner1225@gmail.com>
2023-03-16 13:53:40 +0200

    add second step

---
+++ src/components/BillingAddress.vue
@@ -0,0 +1,74 @@
+<template lang="pug">
+q-card(flat).flex.column
+  .text-h4.sub-title {{ 'Shipping Address' }}
+  .flex.column.q-mt-lg
+    .form-title {{'Country of Origin'}}
+    q-select.q-mt-md(standout v-model="formData.country" :options="options" label="Select your country")
+      template(v-slot:prepend)
+        q-img(src="../assets/map.svg" width="18px" height="18px")
+  .row.justify-between.q-mt-md
+    .flex.column(:style="{maxWidth: '262px', width: '50%'}")
+      .flex.column.form-title {{'First Name *'}}
+      q-input.q-mt-sm(standout v-model="formData.firstName" placeholder="Enter your first name" type="text" :dense="dense")
+        template(v-slot:prepend)
+          q-img(src="../assets/user.svg" width="18px" height="18px")
+    .flex.column(:style="{maxWidth: '262px', width: '50%'}")
+      .form-title {{'Last Name'}}
+      q-input.q-mt-sm(standout v-model="formData.lastName" placeholder="Enter your last name" type="text" :dense="dense")
+        template(v-slot:prepend)
+          q-img(src="../assets/user.svg" width="18px" height="18px")
+  .flex.column.q-mt-sm
+    .form-title {{'Address *'}}
+    q-input.q-mt-sm(standout v-model="formData.address" placeholder="Enter your address" type="text" :dense="dense")
+      template(v-slot:prepend)
+        q-img(src="../assets/home.svg" width="18px" height="18px")
+  .row.justify-between.q-mt-md
+    .flex.column(:style="{maxWidth: '262px', width: '50%'}")
+      .flex.column.form-title {{'Apartment (optional)'}}
+      q-input.q-mt-sm(standout v-model="formData.apartment" placeholder="Enter your apartment" type="text" :dense="dense")
+        template(v-slot:prepend)
+          q-img(src="../assets/building.svg" width="18px" height="18px")
+    .flex.column(:style="{maxWidth: '262px', width: '50%'}")
+      .form-title {{'Suit (optional)'}}
+      q-input.q-mt-sm(standout v-model="formData.suit" placeholder="Enter your suit" type="text" :dense="dense")
+        template(v-slot:prepend)
+          q-img(src="../assets/building.svg" width="18px" height="18px")
+  .row.justify-between.q-mt-md
+    .flex.column(:style="{maxWidth: '262px', width: '50%'}")
+      .flex.column.form-title {{'Post Code'}}
+      q-input.q-mt-sm(standout v-model="formData.code" placeholder="Enter your post code" type="text" :dense="dense")
+        template(v-slot:prepend)
+          q-img(src="../assets/map-marker-plus.svg" width="18px" height="18px")
+    .flex.column(:style="{maxWidth: '262px', width: '50%'}")
+      .form-title {{'City'}}
+      q-select.q-mt-sm(standout v-model="formData.city" :options="options" label="Select your city")
+        template(v-slot:prepend)
+          q-img(src="../assets/home.svg" width="18px" height="18px")
+</template>
+
+<script setup lang="ts">
+import {reactive, ref} from 'vue';
+
+const dense = ref(false);
+const options = ref(['Ukraine', 'USA']);
+const formData = reactive({
+  country: null,
+  firstName: null,
+  lastName: null,
+  address: null,
+  apartment: null,
+  suit: null,
+  code: null,
+  city: null,
+})
+</script>
+
+<style scoped lang="scss">
+.q-field {
+  &::v-deep(.q-field__inner) {
+    .q-field__control {
+      border-radius: 16px;
+    }
+  }
+}
+</style>
--- src/pages/PaymentPage.vue
+++ src/pages/PaymentPage.vue
@@ -20,7 +20,7 @@ q-page(width="100%").row.justify-between.center.payment-content
         .text-h6.q-mb-lg {{ 'Express Checkout' }}
         .q-gutter
           q-btn( v-for="(item, index) in items" :class="[index < 2 ? 'q-mr-md' : '', item]")
-    section(v-if="isFirstStep")
+    section(v-if="isFirstStep").q-mb-xl
       q-card(flat).q-mb-xl.flex.column
         .text-h4.sub-title  {{ 'Contact information' }}
         .flex.column.q-mt-lg
@@ -33,54 +33,41 @@ q-page(width="100%").row.justify-between.center.payment-content
           q-input.q-mt-sm(standout v-model="formData.phone" placeholder="Enter your phone number" type="number" :dense="dense")
             template(v-slot:prepend)
               q-img(src="../assets/phone.svg" width="18px" height="18px")
+      billing-address
+    section(v-if="isSecondStep").second-step
       q-card(flat).q-mb-xl.flex.column
-        .text-h4.sub-title {{ 'Shipping Address' }}
-        .flex.column.q-mt-lg
-          .form-title {{'Country of Origin'}}
-          q-select.q-mt-md(standout v-model="formData.country" :options="options" label="Select your country")
-            template(v-slot:prepend)
-              q-img(src="../assets/map.svg" width="18px" height="18px")
-        .row.justify-between.q-mt-md
-          .flex.column(:style="{maxWidth: '262px', width: '50%'}")
-            .flex.column.form-title {{'First Name *'}}
-            q-input.q-mt-sm(standout v-model="formData.firstName" placeholder="Enter your first name" type="text" :dense="dense")
+        section
+          .text-h4.sub-title.q-mb-lg  {{ 'Payment' }}
+          .q-gutter.payment-btns.q-mb-lg
+            q-btn.credit-card(label="Credit Card" flat bordered)
+            q-btn.paypal(flat label="Paypal")
+          .flex.column.q-mt-lg
+            .form-title {{'Credit Card Number'}}
+            q-input.q-mt-sm(standout v-model="formDataSecond.card" placeholder="2548 9841 2570 ****" type="number" :dense="dense")
+              template(v-slot:prepend)
+                q-img(src="../assets/card.svg" width="18px" height="18px")
+          .flex.column.q-mt-lg
+            .form-title {{'Name on Card'}}
+            q-input.q-mt-sm(standout v-model="formDataSecond.name" placeholder="Enter name on card" type="number" :dense="dense")
               template(v-slot:prepend)
                 q-img(src="../assets/user.svg" width="18px" height="18px")
-          .flex.column(:style="{maxWidth: '262px', width: '50%'}")
-            .form-title {{'Last Name'}}
-            q-input.q-mt-sm(standout v-model="formData.lastName" placeholder="Enter your last name" type="text" :dense="dense")
-              template(v-slot:prepend)
-                q-img(src="../assets/user.svg" width="18px" height="18px")
-        .flex.column.q-mt-sm
-          .form-title {{'Address *'}}
-          q-input.q-mt-sm(standout v-model="formData.address" placeholder="Enter your address" type="text" :dense="dense")
-            template(v-slot:prepend)
-              q-img(src="../assets/home.svg" width="18px" height="18px")
-        .row.justify-between.q-mt-md
-          .flex.column(:style="{maxWidth: '262px', width: '50%'}")
-            .flex.column.form-title {{'Apartment (optional)'}}
-            q-input.q-mt-sm(standout v-model="formData.apartment" placeholder="Enter your apartment" type="text" :dense="dense")
-              template(v-slot:prepend)
-                q-img(src="../assets/building.svg" width="18px" height="18px")
-          .flex.column(:style="{maxWidth: '262px', width: '50%'}")
-            .form-title {{'Suit (optional)'}}
-            q-input.q-mt-sm(standout v-model="formData.suit" placeholder="Enter your suit" type="text" :dense="dense")
-              template(v-slot:prepend)
-                q-img(src="../assets/building.svg" width="18px" height="18px")
-        .row.justify-between.q-mt-md
-          .flex.column(:style="{maxWidth: '262px', width: '50%'}")
-            .flex.column.form-title {{'Post Code'}}
-            q-input.q-mt-sm(standout v-model="formData.code" placeholder="Enter your post code" type="text" :dense="dense")
-              template(v-slot:prepend)
-                q-img(src="../assets/map-marker-plus.svg" width="18px" height="18px")
-          .flex.column(:style="{maxWidth: '262px', width: '50%'}")
-            .form-title {{'City'}}
-            q-select.q-mt-sm(standout v-model="formData.city" :options="options" label="Select your city")
-              template(v-slot:prepend)
-                q-img(src="../assets/home.svg" width="18px" height="18px")
-    section(v-if="isSecondStep")
-      q-card(flat).q-mb-xl.flex.column
-        .text-h4.sub-title  {{ 'Payment' }}
+          .row.justify-between.q-mt-md
+            .flex.column(:style="{maxWidth: '262px', width: '50%'}")
+              .flex.column.form-title {{'Expiry Date'}}
+              q-input.q-mt-sm(standout v-model="formDataSecond.date" placeholder="MM/YY" type="text" :dense="dense")
+                template(v-slot:append)
+                  q-img(src="../assets/calendar.svg" width="18px" height="18px")
+            .flex.column(:style="{maxWidth: '262px', width: '50%'}")
+              .form-title {{'Security Code'}}
+              q-input.q-mt-sm(standout v-model="formDataSecond.cvv" placeholder="CVV" type="number" :dense="dense")
+          q-checkbox.q-mt-md(v-model="formDataSecond.check" label="By checking this box, I acknowledge that I have read and agree to the Terms of Service, and Monthly Billing Terms of this website and want to opt-in for the monthly billed Dream Collection Club®")
+        section.q-mt-xl
+          .text-h4.sub-title.q-mb-xs {{ 'Billing Address' }}
+          .text-subtitle2.sub-text.q-mb-md {{ 'Specify the address for your payment option' }}
+          .flex.column
+            q-radio(v-model="shape" :val="1" label="Same as shipping address" color="black")
+            q-radio(v-model="shape" :val="2" label="Same as shipping address" color="black")
+          billing-address.q-mt-lg(v-if="shape === 2")
     q-btn(@click="onSubmit" label="Continue to Payment" icon-right="east" flat).submit
   .col-xs-12.col-sm-12.col-md-5.col-lg-5.col-xl-5
 </template>
@@ -89,19 +76,19 @@ q-page(width="100%").row.justify-between.center.payment-content
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import {computed, onMounted, reactive, ref} from 'vue';
 import {useQuasar} from 'quasar';
+import BillingAddress from 'components/BillingAddress.vue';
 
 onMounted(() => {
   step.value = 1;
 })
 
+const shape = ref(1);
+
 const items = ['shop', 'paypal', 'google'];
 
-const step = ref(2);
+const step = ref(0);
 const $q = useQuasar();
-
-const accept = ref(false)
 const dense = ref(false);
-const options = ref(['Ukraine', 'USA']);
 
 const isFirstStep = computed(() => {
   return step.value === 1;
@@ -113,14 +100,13 @@ const isSecondStep = computed(() => {
 const formData = reactive({
   email: null,
   phone: null,
-  country: null,
-  firstName: null,
-  lastName: null,
-  address: null,
-  apartment: null,
-  suit: null,
-  code: null,
-  city: null,
+})
+const formDataSecond = reactive({
+  card: null,
+  name: null,
+  date: null,
+  cvv: null,
+  check: false,
 })
 
 const onSubmit = () => {
@@ -211,6 +197,28 @@ const onSubmit = () => {
     border-radius: 16px;
     &::v-deep(.q-icon) {
       font-size: 1.2rem;
+    }
+  }
+  .second-step {
+    .payment-btns {
+      .q-btn {
+        padding: 16px 25px;
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 18px;
+        text-transform: inherit;
+        border-radius: 16px;
+      }
+      .credit-card {
+        color: var(--light-slate-blue);
+        background: rgba(228, 228, 255, 0.1);
+        border: 1px solid var(--light-slate-blue);
+        margin-right: 15px;
+      }
+      .paypal {
+        color: var(--text-color-second);
+        border: 1px solid var(--text-color-second);
+      }
     }
   }
 }
